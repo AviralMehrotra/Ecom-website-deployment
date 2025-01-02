17f277371e5c257b90409b2d8a70927bbb4cf7b4
Aviral Mehrotra <aviralmehrotra2002@hotmail.com>
2025-01-02 10:52:04 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,6 @@
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 import path from "path";
-
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
